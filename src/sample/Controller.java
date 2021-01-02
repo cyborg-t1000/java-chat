db448df198fb13b52b7abe863bf57ca99888fe8e
Maxim Nekrasov <kgtu@mail.ru>
2021-01-02 11:45:44 -0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField textField;
+    @FXML
+    private TextArea textArea;
+}
